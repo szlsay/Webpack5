9da316e452821f5da5e6f95f95d5c2c944080839
小沈曰 <409178030@qq.com>
2022-05-06 22:13:10 +0800

    01-07postcss-preset-env

--- 01WP5/postcss.config.js
+++ 01WP5/postcss.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  plugins: [require('autoprefixer')],
+  // plugins: [require('postcss-preset-env')], // 写法一：默认写法
+  plugins: ['postcss-preset-env'], // 写法二：直接传入字符串，并不是全部的插件都支持
 }
