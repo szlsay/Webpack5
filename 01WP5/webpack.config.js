f6609ca5cfba67dd6ce799ebaa77ceb28457eb27
小沈曰 <409178030@qq.com>
2022-05-06 23:15:37 +0800

    01-09文件配置

--- 01WP5/webpack.config.js
+++ 01WP5/webpack.config.js
@@ -47,7 +47,15 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [{ loader: 'file-loader' }],
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash:8].[ext]',
+              outputPath: 'img',
+            },
+          },
+        ],
       },
     ],
   },
