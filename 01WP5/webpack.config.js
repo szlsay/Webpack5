0cb663e50c5b272c805619c51327a2f33755f1ed
小沈曰 <409178030@qq.com>
2022-05-07 08:45:41 +0800

    02-02HtmlWebpackPlugin

--- 01WP5/webpack.config.js
+++ 01WP5/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
   entry: './src/main.js',
@@ -8,7 +9,12 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     // assetModuleFilename: 'img/[name].[hash:8][ext]',
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'hello say',
+    }),
+  ],
   module: {
     rules: [
       {
